dd19108b628f81d6736ec411fabb73077df113f4
robotics1 <smbeyer88@gmail.com>
2021-09-03 08:38:01 -0600

    Initial working simulation. TODO: Collision avoidance on routes

--- src/hungarian.py
+++ src/hungarian.py
@@ -15,16 +15,6 @@ from nav_msgs.msg import Odometry
 #Robots
 robots = ["usafabot0", "usafabot1", "usafabot2", "usafabot3", "usafabot4", "usafabot5", "usafabot6", "usafabot7", "usafabot8", "usafabot9", "usafabot10", "usafabot11", "usafabot12", "usafabot13", "usafabot14", "usafabot15", "usafabot16", "usafabot17", "usafabot18", "usafabot19", "usafabot20", "usafabot21", "usafabot22", "usafabot23", "usafabot24"]
 
-
-
-#Starting Points
-x_robot = [0, 0, 0, 0, 0, 0, 0.75, 1.50, 2.25, 3.0, 3.75, 4.25, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 0.25, 0.75, 1.50, 2.25, 3.0, 3.75, 4.25]
-y_robot = [0.75, 1.50, 2.25, 3.0, 3.75, 4.25, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 0.75, 1.50, 2.25, 3.0, 3.75, 4.25, 0, 0, 0, 0, 0, 0, 0]
-
-#Destination Points
-x_dest = [1.0, 1.0, 1.0, 1.30, 1.30, 1.60, 2.0, 2.0, 2.0, 2.3, 2.3, 2.6, 3.0, 3.0, 3.0, 3.3, 3.3, 3.3, 3.6, 3.6, 4.0, 4.0, 4.0, 4.6, 4.6]
-y_dest = [3.0, 2.5, 2.0, 2.75, 2.25, 2.5, 3.0, 2.5, 2.0, 3.0, 2.5, 3.0, 3.0, 2.5, 2.0, 3.0, 2.5, 2.0, 3.0, 2.0, 3.0, 2.5, 2.0, 3.0, 2.0]
-
 rows = 25
 cols = 25
 hungarian = np.zeros((rows,cols))
@@ -33,8 +23,8 @@ assignments={}
 
 
 
-def build_hungarian(x_robot, y_robot):
-    for i in range(0, len(robots)): # find the distance for every starting point to each destination
+def build_hungarian(x_robot, y_robot, x_dest, y_dest):
+    for i in range(0, len(x_robot)): # find the distance for every starting point to each destination
         jobs = []
         for j in range(0, len(x_dest)):
             distance = ((x_dest[j] - x_robot[i]) ** 2 + (y_dest[j] - y_robot[i]) ** 2) ** 0.5
@@ -48,7 +38,7 @@ def build_hungarian(x_robot, y_robot):
     for robot, col in zip(robots, col_ind):
         assignments[robot]=[x_dest[col],y_dest[col]]
         
-    print(assignments)
+    return assignments
 
 
 if __name__ == '__main__':
--- src/master.py
+++ src/master.py
@@ -1,40 +1,27 @@
 #!/usr/bin/env python3
-"""----------------------------------------------------------------------------------
-Ground and Air Robot Teaming Capstone: Ground Controller
-Date: 10 Feb 2020
-----------------------------------------------------------------------------------"""
 
 # Import important libraries
-import roslib
 import rospy
 import time
-import serial
-import array
-import hungarian
-from std_msgs.msg import String
-from threading import Thread
+from hungarian import build_hungarian
 
-from geometry_msgs.msg import Point
-from geometry_msgs.msg import Pose
-from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose
 
 # TODO: take current robot position instead of assigning initial positions - fixed
 # TODO: what if you have more or less robots than needed? 
 
-# Global Variables
-listX = []
-listY = []
-xrobot = []
-yrobot = []
-robotDestination = []
 
 # TODO: change this list to match number of robots
 #must match number of robots entered by user
 robots = ['usafabot0', 'usafabot1', 'usafabot2', 'usafabot3', 'usafabot4',
           'usafabot5', 'usafabot6', 'usafabot7', 'usafabot8', 'usafabot9',
           'usafabot10', 'usafabot11', 'usafabot12', 'usafabot13', 'usafabot14',
-          'usafabot15', 'usafabot16', 'usafabot18', 'usafabot19',
+          'usafabot15', 'usafabot16', 'usafabot17', 'usafabot18', 'usafabot19',
           'usafabot20', 'usafabot21', 'usafabot22', 'usafabot23', 'usafabot24']
+
+#Destination Points
+x_dest = [1.0, 1.0, 1.0, 1.30, 1.30, 1.60, 2.0, 2.0, 2.0, 2.3, 2.3, 2.6, 3.0, 3.0, 3.0, 3.3, 3.3, 3.3, 3.6, 3.6, 4.0, 4.0, 4.0, 4.6, 4.6]
+y_dest = [3.0, 2.5, 2.0, 2.75, 2.25, 2.5, 3.0, 2.5, 2.0, 3.0, 2.5, 3.0, 3.0, 2.5, 2.0, 3.0, 2.5, 2.0, 3.0, 2.0, 3.0, 2.5, 2.0, 3.0, 2.0]
 
 # Define the Controller class
 class Master:
@@ -42,34 +29,26 @@ class Master:
     def __init__(self, USAFABOT):
         self.curr_pos = Pose()
         self.dest_pos = Point()
-        self.USAFABOT = USAFABOT
+        self.name = USAFABOT
 
         # -----------------------------------------------------------------------------
         # Topics and Timers 
         # -----------------------------------------------------------------------------      
         # Publish to the controller
-        self.pub = rospy.Publisher(USAFABOT + '/dest_pos', Point, queue_size = 10)
-        rospy.Timer(rospy.Duration(.1), self.callback_posTalker)
+        self.pub = rospy.Publisher(self.name + '/dest_pos', Point, queue_size = 10)
         # Listen for the bots' current position to the controller
-        rospy.Subscriber(USAFABOT + '/curr_pos', Pose, self.callback_posListener)
+        rospy.Subscriber(self.name + '/curr_pos', Pose, self.callback_currPos)
         #rospy.Subscriber(USAFABOT + '/odom', Odometry, self.callback_groundListener)
 
     #-------------------------------------------------------------------------------
     # Class Functions
     #-------------------------------------------------------------------------------
-    def callback_posTalker(self, data):
-        # message is printed to the terminal, written to Node log file, and written to rosout
-        #rospy.loginfo(self.curr_pos)
-        self.pub.publish(self.curr_pos) # work being done
-        #rate.sleep()
+    def callback_currPos(self, data):
+        self.curr_pos.position.x = round(data.position.x, 3)
+        self.curr_pos.position.y = round(data.position.y, 3)
+        self.curr_pos.orientation.z = round(data.orientation.z, 3)
 
-    def callback_posListener(self, data):
-        self.curr_pos.position.x = data.position.x
-        self.curr_pos.position.y = data.position.y
-        self.curr_pos.orientation.z = data.orientation.z
-        #rospy.loginfo(rospy.get_caller_id() + 'I heard position:%s' %(self.gnd_curr))
-
-    def getCurrentPos(self):
+    def getCurrPos(self):
         return self.curr_pos.position.x, self.curr_pos.position.y;
 
     def setDestPosition(self, x, y):
@@ -89,37 +68,42 @@ if __name__ == '__main__':
     for k in robots:
         bots.append(Master(k))
 
+
+    # Global Variables
+    xrobot = []
+    yrobot = []
+
     # Get initial bot positions
     for bot in bots:
         x = 0
         y = 0
         # block until bot's RR is operational
         while(x == 0 and y == 0):
-            x,y = bot.getCurrentPos()
+            x,y = bot.getCurrPos()
         xrobot.append(x)
         yrobot.append(y)
-        print("Completed bot: " + bot.USAFABOT)
+        print("Completed bot: " + bot.name)
 
     # Calculation functions
-    coordList = build_hungarian(xrobot, yrobot)
-
-
-
-    print("x = ", listX)
-    print("y = ", listY)
+    print(xrobot)
+    print(yrobot)
+    coordList = build_hungarian(xrobot, yrobot, x_dest, y_dest)
+    
     print("Comined coordList = ", coordList)
 
     print("Robot Init X = ", xrobot)
     print("Robot Init Y = ", yrobot)
-    print("Combined RobotDestination = ", robotDestination)
 
     # Assign final bot destinations
     i = 0
 
     for bot in bots:
-        bot.setDestPosition(x_dest[i], y_dest[i])
-        curr_x,curr_y = bot.getCurrentPos()
+        bot.setDestPosition(coordList[bot.name][0], coordList[bot.name][1])
+        bot.pub.publish(bot.dest_pos)
+        print(bot.dest_pos)
+        curr_x,curr_y = bot.getCurrPos()
         init_dist = ((x_dest[i] - curr_x) ** 2 + (y_dest[i] - curr_y) ** 2) ** 0.5        
+    
 
     rospy.spin()
 
