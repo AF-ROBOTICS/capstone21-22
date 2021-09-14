72f9ccc69d8d0611c9e57fea33615a155d074201
robotics2 <robotics2@robotics2.hsd1.co.comcast.net>
2021-09-14 11:24:48 -0600

    AJT commented potential_field code

--- src/potential_field.py
+++ src/potential_field.py
@@ -15,34 +15,38 @@ robots = ['usafabot0', 'usafabot1', 'usafabot2', 'usafabot3', 'usafabot4',
           
 def potential_field(xrobot, yrobot, coordList):
 
-	destmult = 25
-	potPointX = []
+	destmult = 25 # Multiplier to pull robot to destination stronger than repulsion from other bots
+	potPointX = [] # Vector of rounded distances between each robot and its destination
 	potPointY = []
-	potBotX = []
+	potBotX = [] # Vector of rounded distances between each robot to eachother
 	potBotY = []
-	vectorX = []
+	vectorX = [] # Vector of each robots total vector i.e combination of destination and obstacles 
 	vectorY = []
-	vector = (vectorX, vectorY)
+	vector = (vectorX, vectorY) # Is this used??
 	
 	for bot in range(0, len(xrobot)):
-
-		pointDistX = coordList[robots[bot]][0]-xrobot[bot]
+		# Find current distance componentwise between robot and destination
+		pointDistX = coordList[robots[bot]][0]-xrobot[bot] 
 		pointDistY = coordList[robots[bot]][1]-yrobot[bot]
 		
-		if(pointDistX >= 0):
+		
+		# Append each individual distance to the array
+		if(pointDistX > 0):
 			potPointX.append(round(1/(pointDistX ** 2), 3))
 		else:
 			potPointX.append(-1*round(1/(pointDistX ** 2), 3))
 			
-		if(pointDistY >= 0):
+		if(pointDistY > 0):
 			potPointY.append(round(1/(pointDistY ** 2), 3))
 		else:
 			potPointY.append(-1*round(1/(pointDistY ** 2), 3))
+			
+		# Find the distance of each robot to each other
 		
 		for i in range(0, len(xrobot)):
 				distanceX = round(xrobot[i] - xrobot[bot], 3)
 				distanceY = round(yrobot[i] - yrobot[bot], 3)
-
+				# Check that robot is not repelling itself
 				if (distanceX != 0):
 					if (distanceX > 0):
 						potBotX.append(-1*(round((1/(distanceX**2)), 3)))
@@ -58,7 +62,7 @@ def potential_field(xrobot, yrobot, coordList):
 						potBotY.append((round((1/(distanceY**2)), 3)))
 				else:
 					potBotY.append(0)	
-
+		# 
 		vectorX.append(sum(potBotX) + destmult*potPointX[bot])
 		vectorY.append(sum(potBotY) + destmult*potPointY[bot])
 		
