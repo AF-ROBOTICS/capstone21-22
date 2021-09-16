86a86cb7e00cbc5e275267784d5ff50fb8897796
CaseyGymnast <C22Cason.Couch@usafa.edu>
2021-09-16 10:55:48 -0600

    Added three new files for AI implementation

---
+++ src/Driver.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Driver.py - Robotic Teaming Artificial Intelligence
+# Author: Cason Couch
+# Description: This file directs individual driving instructions to
+#   robots using the routes produced in Routing.py
+# Subscriptions: rostopic '/routes' (from Routing.py)
+# Publications: rostopic '/drivers' (to Robot_Driver.py)
---
+++ src/Routing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# Routing.py - Robotic Teaming Artificial Intelligence
+# Author: Cason Couch
+# Description: This file maps routes for bots to take based on the
+#   mapping produced in Global_Env.py
+# Subscriptions: rostopic '/global_env' (from Global_Env.py)
+# Publications: rostopic '/routes' (to Driver.py)
