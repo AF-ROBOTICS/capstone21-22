d48e975670693e2db99b8db50646cde67b1e3fb3
C22Anthony.Tolbert <c22anthony.tolbert@usafa.edu>
2022-01-12 13:41:51 -0700

    AJT, log to fstrings

--- src/waterfall.py
+++ src/waterfall.py
@@ -17,14 +17,14 @@ BASENAME = 'usafabot'
 
 def ctrl_c_handler(signum, frame):
     print('\n')
-    stop_bots()
+    stop_bots(bots)
     logger.info("KILLED with CTRL_C")
     exit(1)
 
 
+bots = []
 signal.signal(signal.SIGINT, ctrl_c_handler)
 if __name__ == '__main__':
-    bots = []
     rospy.init_node('master', anonymous=True)
     # fill list of Master class bots
     for i in range(0, NUM_BOTS):
@@ -37,7 +37,7 @@ if __name__ == '__main__':
             while not bot.close:  # TODO: How to not busy wait here
                 pass
         else:
-            logger.info("Skipping", bot.name)
+            logger.info(f"Skipping f{bot.name}")
 
     logger.info("all bots complete")
     stop_bots(bots)
